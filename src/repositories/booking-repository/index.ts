1dc4ea5833707b9cad9a0b12fe91495391c762a2
Guilfornari <guilfornari@gmail.com>
2023-07-22 14:50:18 -0300

    trying to fix querys

--- src/repositories/booking-repository/index.ts
+++ src/repositories/booking-repository/index.ts
@@ -11,7 +11,7 @@ async function getBookings(): Promise<BookingWithRooms[]> {
     });
 }
 
-async function makeBooking(roomId: number, userId: number): Promise<Booking> {
+async function makeBooking(roomId: number, userId: number) {
     const booking: BookingParams = { userId, roomId };
     return prisma.booking.create({
         data: booking
@@ -24,14 +24,14 @@ async function getBookingsByRoomId(roomId: number) {
     });
 }
 
-async function updateBooking(roomId: number, bookingId: number): Promise<Booking> {
+async function updateBooking(roomId: number, bookingId: number) {
     return prisma.booking.update({
         where: { id: bookingId },
         data: { roomId }
     });
 }
 
-async function getBookingsByUserId(userId: number) {
+async function getBookingsByUserId(userId: number): Promise<Booking> {
     return prisma.booking.findFirst({
         where: { userId }
     });
